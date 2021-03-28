3a2eaf9ccec7f0873d2cb4a4f2014895ee3ea456
Sagar Dev Maity <maity.dev.sagar@gmail.com>
2021-03-28 19:45:03 +0530

    created pagination component with simple navigation funtionality within the first 100 data returned by the inital api call

--- src/components/bookSearch.js
+++ src/components/bookSearch.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BookTable from './bookTable';
 
 
+//Component for the Search bar and Search button
 function Searchbar(props) {
     const [searchKey, setSearchKey] = React.useState('');
     function handleChange(e) {
@@ -33,17 +34,43 @@ function Searchbar(props) {
     );
 }
 
+//Component for pagnation portion of the app
+function Pagination(props) {
+    return (
+        <div className="pagination">
+            {props.pageNo > 1 && <button onClick={() => props.handlePaginationClick(false)}>Prev</button>}
+            <button>{props.pageNo}</button>
+            {props.pageNo < props.maxPageNo && <button onClick={props.handlePaginationClick}>Next</button>}
+        </div>
+    );
+}
+
 export default function BookSearch(props) {
+    const pageDataLength = 10; //showing 10 data per page
     const [result, setResult] = React.useState(null);
+    const [pageNo, setPageNo] = React.useState(1);
     const [loading, setLoading] = React.useState(false);
+
+    function handlePaginationClick(next = true) {
+        if (next) {
+            setPageNo(pageNo + 1);
+        } else {
+            setPageNo(pageNo - 1)
+        }
+    }
 
     return (
         <div>
             <h1>Open Library Book Search</h1>
             <Searchbar setResult={setResult} setLoading={setLoading}/>
-            {loading ? (<h1>Loading...</h1>) : (result && (result.docs.length ? (
-                <BookTable books={result.docs.slice(0, 11)}/>
-            ) : <h2>No Result Found</h2>))}
+            {loading ? (<h1>Loading...</h1>) : (result && (result.docs.length ? (<>
+                <BookTable books={result.docs.slice((pageNo-1)*pageDataLength, pageNo*pageDataLength)}/>
+                <Pagination 
+                    maxPageNo={Math.ceil(+result.numFound/pageDataLength)} 
+                    pageNo={pageNo}
+                    handlePaginationClick={handlePaginationClick}
+                />
+            </>) : <h2>No Result Found</h2>))}
         </div>
     );
 }
